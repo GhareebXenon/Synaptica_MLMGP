2d3f031543c381e3ea67c2c648fa3a98346e1cf1
mostafa-mohamed7 <mossaber777@gmail.com>
2024-02-08 21:17:14 +0200

    laser puzzle

---
+++ Synaptica_MLMGP/Assets/Scripts/LaserReflector.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LaserReflector : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    float rotationSpeed = 0.2f;
+
+    Vector3 position;
+    Vector3 direction;
+    LineRenderer lr;
+    public bool isOpen;
+   [SerializeField] private UnityEvent OnCompleted;
+
+    GameObject tempReflector;
+    void Start()
+    {
+        isOpen = false; //initailze lazer as closed
+        lr = gameObject.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        if (isOpen)
+        { // activate the laser
+            lr.positionCount = 2;// set the laser line
+            lr.SetPosition(0, position);//start point
+            RaycastHit hit;// check for collision 
+            if (Physics.Raycast(position, direction, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.CompareTag("Reflector"))
+                { // if hit reflector
+                    // interact
+                    tempReflector = hit.collider.gameObject;
+                    Vector3 temp = Vector3.Reflect(direction, hit.normal);
+                    hit.collider.gameObject.GetComponent<LaserReflector>().OpenRay(hit.point, temp);
+                }
+               if (hit.collider.CompareTag("CenterReflector"))
+                { // if hit reflector
+                  // interact
+                    OnCompleted?.Invoke();
+                    tempReflector = hit.collider.gameObject;
+                    Vector3 temp = Vector3.Reflect(direction, hit.normal);
+                    hit.collider.gameObject.GetComponent<LaserReflector>().OpenRay(hit.point, temp);
+                    
+                    
+                }
+                lr.SetPosition(1, hit.point);// set laser to the collosion point 
+            }
+            
+            else
+            {
+                if (tempReflector)
+                { // if no collosion
+                    // deactive
+                    tempReflector.GetComponent<LaserReflector>().CloseRay();
+                    tempReflector=null;
+                }
+                lr.SetPosition(1,direction*100);
+            }
+        }
+        else
+        {
+            if (tempReflector)
+            {
+                tempReflector.GetComponent<LaserReflector>().CloseRay();
+                tempReflector=null;
+            }
+        }
+        
+    }
+    // open and close laser
+    public void OpenRay(Vector3 pos,Vector3 dir)
+    {
+        isOpen = true;
+        position = pos;
+        direction = dir;
+    }
+    public void CloseRay()
+    {
+        isOpen = false;
+        lr.positionCount = 0;
+    }
+    // mouse rotation
+    void OnMouseDown()
+    { // calc intailzation mouse for dragging
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);    
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    
+    }
+ // update position based on mouse movement
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);  
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+        //rotate cube
+        float XaxisRotation = Input.GetAxis("Mouse X")*rotationSpeed;
+		float YaxisRotation = Input.GetAxis("Mouse Y")*rotationSpeed;
+		transform.RotateAround(Vector3.down, XaxisRotation);
+		transform.RotateAround(Vector3.right, YaxisRotation);
+    
+    }
+}
---
+++ Synaptica_MLMGP/Assets/Scripts/LaserSource.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSource : MonoBehaviour
+{
+    [SerializeField] Transform laserStartPoint;
+    Vector3 direction;
+    LineRenderer lr;// draw the laser beam
+    GameObject tempReflector;// reflector
+    void Start()
+    {
+        lr = gameObject.GetComponent<LineRenderer>();
+        direction = laserStartPoint.forward;// starting direction
+        lr.positionCount = 2;// set the lr with two points 
+        lr.SetPosition(0, laserStartPoint.position);//position the start point
+    }
+    void Update()
+    {
+        RaycastHit hit;//shoots ray from statring direction to check for collision
+        if(Physics.Raycast(laserStartPoint.position,direction,out hit, Mathf.Infinity))
+        {
+            if (hit.collider.CompareTag("Reflector"))
+            {
+                tempReflector = hit.collider.gameObject;
+                Vector3 temp = Vector3.Reflect(direction, hit.normal);// calc the new direction
+                hit.collider.gameObject.GetComponent<LaserReflector>().OpenRay(hit.point, temp);// activate the reflector
+            }
+            lr.SetPosition(1, hit.point);// set the end point of the laser to colloision point
+        }
+        else
+        {
+            //if no collosion close hit reflector
+            if (tempReflector)
+            {
+                tempReflector.GetComponent<LaserReflector>().CloseRay();
+                tempReflector=null;
+            }
+            lr.SetPosition(1, direction * 200);// laser extend to staright line
+        }
+    }
+}
