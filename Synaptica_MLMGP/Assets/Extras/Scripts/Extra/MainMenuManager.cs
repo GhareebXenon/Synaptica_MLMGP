256ab0a767295978d31c80243c7cec1f918a5249
GhareebXenon <108562554+GhareebXenon@users.noreply.github.com>
2024-02-23 20:04:15 +0200

    UI update

--- Synaptica_MLMGP/Assets/Extras/Scripts/Extra/MainMenuManager.cs
+++ Synaptica_MLMGP/Assets/Extras/Scripts/Extra/MainMenuManager.cs
@@ -1,33 +1,43 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 namespace cowsins {
-public class MainMenuManager : MonoBehaviour
-{
-    [SerializeField] private CanvasGroup introMenu;
-
-    private bool canLerp = false, waitToLerp;
-
-    private CanvasGroup from, to;
-
-    private void Update()
+    public class MainMenuManager : MonoBehaviour
     {
-        if (!canLerp) return;
+        [SerializeField] private CanvasGroup introMenu;
 
-        to.gameObject.SetActive(true); 
-        from.alpha -= Time.deltaTime;
-        if(from.alpha <= .2f && waitToLerp || !waitToLerp) to.alpha += Time.deltaTime;
+        private bool canLerp = false, waitToLerp;
 
-        if (introMenu.alpha == 0) 
-        from.gameObject.SetActive(false);
+        private CanvasGroup from, to;
+        private GameObject View;
+
+        private void Update()
+        {
+            if (!canLerp) return;
+
+            to.gameObject.SetActive(true);
+            from.alpha -= Time.deltaTime;
+            if (from.alpha <= .2f && waitToLerp || !waitToLerp) to.alpha += Time.deltaTime;
+
+            if (introMenu.alpha == 0)
+                from.gameObject.SetActive(false);
+        }
+        public void ChangeMenu() => canLerp = true;
+
+        public void SetFrom(CanvasGroup From) => from = From;
+
+        public void SetTo(CanvasGroup To) => to = To;
+
+        public void WaitToLerp(bool boolean) => waitToLerp = boolean;
+
+        public void ChangeScene(int scene) => SceneManager.LoadScene(scene);
+
+        public void DeActivate(GameObject view) 
+        {
+            
+            view.SetActive(false);
+        }
+        public void Activate(GameObject view) =>view.SetActive(true);
+
+
     }
-    public void ChangeMenu() => canLerp = true;
-
-    public void SetFrom(CanvasGroup From) => from = From;
-
-    public void SetTo(CanvasGroup To) => to = To;
-
-    public void WaitToLerp(bool boolean) => waitToLerp = boolean;
-
-    public void ChangeScene(int scene) => SceneManager.LoadScene(scene); 
-}
 }
