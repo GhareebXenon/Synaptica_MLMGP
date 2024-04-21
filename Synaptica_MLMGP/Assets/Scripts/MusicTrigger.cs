c1b73194d88ff4d2cbe7ee0effd594a4162542ca
Triple A <abdulrahman.a.m.a.k.160@gmail.com>
2024-04-21 20:38:46 +0200

    Music Triggers

--- Synaptica_MLMGP/Assets/Scripts/MusicTrigger.cs
+++ Synaptica_MLMGP/Assets/Scripts/MusicTrigger.cs
@@ -2,14 +2,24 @@ using cowsins;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 public class MusicTrigger : MonoBehaviour
 {
+    [Header("Clips")]
     [SerializeField] private AudioClip musicOld;
     [SerializeField] private AudioClip musicNew;
+    [SerializeField][Tooltip("Plays after Music New, leave it Null if playing one track.")] private AudioClip musicAfter;
+
+    [Header("Attributes for Music New")]
     [SerializeField] private float transitionTime = 1.5f;
     [SerializeField] private float volume = 0.25f;
     [SerializeField] private bool loopable = true;
+
+    [Header("Attributes for Music After")]
+    [SerializeField] private float transitionTimeAfter = 1.5f;
+    [SerializeField] private float volumeAfter = 0.25f;
+    [SerializeField] private bool loopableAfter = true;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,8 +30,15 @@ public class MusicTrigger : MonoBehaviour
 
             if (musicNew != null && musicNew != musicCurr)
             {
-                SoundManager.Instance.PlayMusicFadeIn(musicNew, volume, transitionTime, loopable);
-                Debug.LogWarning($"Music changed to '{musicNew.name}'.");
+                if (musicAfter == null)
+                {
+                    SoundManager.Instance.PlayMusicFadeIn(musicNew, volume, transitionTime, loopable);
+                    Debug.LogWarning($"Music changed to '{musicNew.name}'.");
+                }
+                else
+                {
+                    StartCoroutine(PlayMusicAfter());
+                }
             }
         }
     }
@@ -39,4 +56,22 @@ public class MusicTrigger : MonoBehaviour
     //        }
     //    }
     //}
+
+    IEnumerator PlayMusicAfter()
+    {
+        yield return null;
+
+        AudioSource source = SoundManager.Instance.GetMusicSource();
+
+        SoundManager.Instance.PlayMusicFadeIn(musicNew, volume, transitionTime, loopable);
+        Debug.LogWarning($"Music changed to '{musicNew.name}'.");
+
+        while (source.isPlaying)
+        {
+            yield return null;
+        }
+
+        SoundManager.Instance.PlayMusicFadeIn(musicAfter, volumeAfter, transitionTimeAfter, loopableAfter);
+        Debug.LogWarning($"Music changed to '{musicAfter.name}' after {musicNew.name} has finished.");
+    }
 }
