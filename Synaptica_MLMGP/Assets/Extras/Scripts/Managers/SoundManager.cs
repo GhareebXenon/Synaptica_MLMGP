6db2f5e1e464a648625f47ee4de286d7d09e0a5d
Triple A <abdulrahman.a.m.a.k.160@gmail.com>
2024-06-25 14:36:41 +0300

    Added Game Icon & Custom Cursor + Some Other Enhancements

--- Synaptica_MLMGP/Assets/Extras/Scripts/Managers/SoundManager.cs
+++ Synaptica_MLMGP/Assets/Extras/Scripts/Managers/SoundManager.cs
@@ -54,13 +54,13 @@ namespace cowsins
                     PlayMusicFadeIn(bgm[1], 1.0f, 0.99f);
                     break;
                 case "Level 1":
-                    PlayMusicFadeIn(bgm[2], 0.6f, 0.9f);
+                    PlayMusicFadeIn(bgm[2], 0.69f, 0.9f);
                     break;
                 case "Level 2":
                     PlayMusicFadeIn(bgm[3], 0.2f, 0.5f);
                     break;
                 case "Level 3":
-                    PlayMusicFadeIn(bgm[2], 0.6f, 0.8f);
+                    PlayMusicFadeIn(bgm[2], 0.69f, 0.8f);
                     break;
                 case "Level 4":
                     PlayMusicFadeIn(bgm[4], 0.24f, 0.1f);
