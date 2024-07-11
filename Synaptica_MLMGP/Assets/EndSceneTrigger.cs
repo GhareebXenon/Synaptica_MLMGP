a046777f5ebcbc277095bc7d1b4cdf091756eb16
GhareebXenon <108562554+GhareebXenon@users.noreply.github.com>
2024-07-12 01:14:14 +0300

    PreEnd Scene Time Line
    -Updated Dialogue and SFX
    -Added two new Timelines and Updated and older one.

--- Synaptica_MLMGP/Assets/EndSceneTrigger.cs
+++ Synaptica_MLMGP/Assets/EndSceneTrigger.cs
@@ -1,17 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class EndSceneTrigger : MonoBehaviour
 {
     [SerializeField] private Animator doorAnimator;
     [SerializeField] private GameObject generalManagers;
+    [SerializeField] private PlayableDirector cutScene;
+    [SerializeField] private PlayableDirector cutScene1;
+    [SerializeField] private BlazeAI blazeAI;
     private bool entered = false;
 
     private void Start()
     {
         generalManagers = GameObject.Find("GeneralManagers");
+       
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,10 +33,16 @@ public class EndSceneTrigger : MonoBehaviour
     IEnumerator Countdown()
     {
         Debug.Log("Countdown Started!");
+        yield return new WaitForSeconds(20);
+        cutScene1.Play();
         yield return new WaitForSeconds(90);
+        blazeAI.enabled = false;
+        cutScene.Play();
+        yield return new WaitForSeconds(13);
         Debug.Log("Countdown Ended!");
         Destroy(generalManagers);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         yield return null;
     }
+   
 }
